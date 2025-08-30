dad6cabe6adeba82206a425a72f81f3b183e9b61
mfaizan911 <mfaizan.webcraft@gmail.com>
2025-08-31 04:16:49 +0500

    dark-mode

--- potfolio/app/components/Navbar.jsx
+++ potfolio/app/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { assets } from "@/assets/assets";
 import Image from "next/image";
 import React, { useRef } from "react";
 
-const Navbar = () => {
+const Navbar = ({ isDarkMode, setIsDarkMode }) => {
 
   const sideMenuRef = useRef();
   const openMenu = () => {
@@ -15,13 +15,14 @@ const Navbar = () => {
 
   return (
     <>
-      <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]">
+      <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%] dark:hidden">
         <Image src={assets.header_bg_color} alt="" className="w-full" />
       </div>
       <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50'>
         <a href="#top">
-          <Image src={assets.logo} alt="" className="w-28 cursor-pointer mr-14 bg-white/50 backdrop-blur-lg shadow-sm py-2 px-3 border rounded-full" /> </a>
-        <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-2 bg-white/50 backdrop-blur-lg shadow-sm'>
+          <Image src={isDarkMode ? assets.logo_dark : assets.logo} alt="" className="w-28 cursor-pointer mr-14 bg-white/50 backdrop-blur-lg shadow-sm py-2 px-3 border rounded-full dark:border dark:border-white/50 dark:bg-transparent" />
+        </a>
+        <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-2 bg-white/50 backdrop-blur-lg shadow-sm dark:border dark:border-white/50 dark:bg-transparent'>
           <li> <a className="font-Ovo" href="#top"> Home </a> </li>
           <li> <a className="font-Ovo" href="#about"> About </a> </li>
           <li> <a className="font-Ovo" href="#services"> Services </a> </li>
@@ -30,23 +31,23 @@ const Navbar = () => {
         </ul>
 
         <div className="flex items-center gap-4">
-          <button className="px-2 py-2 bg-white/50 backdrop-blur-lg shadow-sm rounded-full cursor-pointer">
-            <Image className="w-6" src={assets.moon_icon} alt="" />
+          <button onClick={() => setIsDarkMode(prev => !prev)} className="px-2 py-2 bg-white/50 backdrop-blur-lg shadow-sm rounded-full cursor-pointer dark:border dark:border-white/50 dark:bg-transparent">
+            <Image className="w-6" src={isDarkMode ? assets.sun_icon : assets.moon_icon} alt="" />
           </button>
-          <a className="hidden lg:flex items-center gap-3 px-8 py-2 border border-gray-500 rounded-full ml-4 font-Ovo bg-white/50 backdrop-blur-lg shadow-sm" href="#contact" >
-            Contact <Image src={assets.arrow_icon} alt="" className="w-3" /> </a>
+          <a className="hidden lg:flex items-center gap-3 px-8 py-2 border border-gray-500 rounded-full ml-4 font-Ovo bg-white/50 backdrop-blur-lg shadow-sm dark:border dark:border-white/50 dark:bg-transparent" href="#contact" >
+            Contact <Image src={isDarkMode ? assets.arrow_icon_dark : assets.arrow_icon} alt="" className="w-3" /> </a>
 
           <button onClick={openMenu} className="block md:hidden ml-3 cursor-pointer">
-            <Image className="w-6" src={assets.menu_black} alt="" />
+            <Image className="w-6" src={isDarkMode ? assets.menu_white : assets.menu_black} alt="" />
           </button>
         </div>
 
         {/* mobile menu */}
 
-        <ul ref={sideMenuRef} className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50h-screen bg-rose-50 transition duration-600">
+        <ul ref={sideMenuRef} className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50h-screen bg-rose-50 transition duration-600 dark:bg-[#2A004A] dark:text-white">
 
           <div className="absolute right-6 top-6" onClick={closeMenu}>
-            <Image className="w-5 cursor-pointer" src={assets.close_black} alt="" />
+            <Image className="w-5 cursor-pointer" src={isDarkMode ? assets.close_white : assets.close_white} alt="" />
           </div>
 
           <li> <a className="font-Ovo" onClick={closeMenu} href="#top">Home</a> </li>
--- potfolio/tailwind.config.mjs
+++ potfolio/tailwind.config.mjs
@@ -1,25 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    ['./**/*.{js,ts,jsx,tsx,mdx}'
-
-  ],
+  content:
+    [
+      './**/*.{js,ts,jsx,tsx,mdx}'
+    ],
   theme: {
     extend: {
-        colors: {
-            darkHover: '#2A004A',
-            darkTheme: '#11001F',
-        },
-        fontFamily: {
-            Outfit: ["var(--font-outfit)", "sans-serif"],
-            Ovo: ["var(--font-ovo)", "serif"]
-        },
-        boxShadow: {
-          'black' : '4px 4px 0 #000',
-          'white' : '4px 4px 0 #fff',
-        }
+      colors: {
+        darkHover: '#2A004A',
+        darkTheme: '#11001F',
+      },
+      fontFamily: {
+        Outfit: ["var(--font-outfit)", "sans-serif"],
+        Ovo: ["var(--font-ovo)", "serif"]
+      },
+      boxShadow: {
+        'black': '4px 4px 0 #000',
+        'white': '4px 4px 0 #fff',
+      }
     },
   },
-  darkMode: 'class',
+  darkMode: "class",
   plugins: [],
 }
